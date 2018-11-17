301bd2181cfe09d62ba2ca84aa7ce96d53ff899e
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-11-16 21:36:30 -0500

    Taking in input and storing it in the allocated space for string

--- project2.s
+++ project2.s
@@ -5,6 +5,10 @@
 	long: .asciiz "Input is too long."
 .text                           # Assembly language instructions
 main:
-
+	li $v0, 8  #  Taking in input
+	la $a0, userInput  #  load byte space into address
+	li $a1, 10000  #  allot the byte space for string
+	syscall
+	
 	li $v0, 10                  # system call code for exit = 10
 	syscall
