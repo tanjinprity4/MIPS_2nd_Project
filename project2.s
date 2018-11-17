db0c4960fe12db28ad4bb6ae631e9244c15b3e13
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-11-16 21:33:46 -0500

    declaring string variable names used in program; storage allocated in main memory

--- project2.s
+++ project2.s
@@ -1,5 +1,8 @@
 .data
-
+	userInput: .space 10000
+	empty: .asciiz "Input is empty."
+	invalid: .asciiz "Invalid base-36 number."
+	long: .asciiz "Input is too long."
 .text                           # Assembly language instructions
 main:
 
