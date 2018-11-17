15025c48209e78c117cb3d460ab7d1f405a13e7a
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-11-16 21:38:13 -0500

    Moving user input to two different registers  and  for use

--- project2.s
+++ project2.s
@@ -9,6 +9,8 @@ main:
 	la $a0, userInput  #  load byte space into address
 	li $a1, 10000  #  allot the byte space for string
 	syscall
+	move $t0, $a0  #  move user input to $t0
+	move $t7, $a0  #  move user input in another register, $t7 for later
 	
 	li $v0, 10                  # system call code for exit = 10
 	syscall
