65e20a414f9927623c6e2a9bf73aab7af9aad6b5
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-11-16 22:34:18 -0500

    check for an lower case.

--- project2.s
+++ project2.s
@@ -84,6 +84,12 @@ char_upper:
 	bne $t2, 1, check_prev
 	li $t4, 1
 	j loop
+
+char_lower:
+	addi $s1, $s1, 1  #  increment for valid character count
+	addi $t2, $t2, 1  #  increment for valid character count
+	bne $t2, 1, check_prev
+	li $t4, 1
 	
 exit:
 	li $v0, 10                  # system call code for exit = 10
